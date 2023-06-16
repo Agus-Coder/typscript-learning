49631dd4f8282aefbaad1c50d7393e41f9bc7a21
August <93748858+Agus-Coder@users.noreply.github.com>
2023-06-16 16:58:23 +0000

    new files

--- TS-course2-5.ts
+++ TS-course2-5.ts
@@ -2,11 +2,23 @@
 
 interface Contacto {
     name: string,
-    tel: number
+    tel: number,
+    //method inside an interface
+    // clone() : Contacto
 }
 
+//The following line, we are adding types to parameters, but not into the function
 function clone(source: Contacto) {
     return Object.apply({}, source)
 }
 
-// simply adding :Contact will restrict the type of the parameter
+// Then, we must do the following
+function trueClone(source: Contacto): Contacto {
+    return Object.apply({}, source)
+}
+
+const a: Contacto = {name: 'Homero Simpson', tel: 1234}
+const b = trueClone(a)
+
+
+// We can also add the method inside the function
